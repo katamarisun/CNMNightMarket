3a0c656b667210420fc7060ce08b3f2bf4d25cca
Jacob Holesinger <jakeholesinger@gmail.com>
2019-02-28 01:12:30 -0800

    putting Jackies stall back

--- assets/SetProps/Stalls/Jackies_stall_latest.ma
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:144ebd19582b35b0cd2d1af7766e6fc5c046ea012f847316a86ad3583f9721e5
-size 209
--- scripts/clear_licenses.py
+++ scripts/clear_licenses.py
@@ -11,15 +11,25 @@ for root, dirs, files in os.walk(directory, topdown=False):
 
 file_count = 0
 for file in maya_files:
-	with open(file,"r+") as f:
+	hasLicense = False
+	with open(file, "r") as f:
 		new_f = f.readlines()
 		f.seek(0)
 		for line in new_f:
-			if "license" not in line:
-				#added to purge arnold :)
-				if "mtoa" not in line:
-					f.write(line)
-		f.truncate()
-	print("Cleared a license from: ", file, "\n")
-	file_count += 1
+			if "lecense" in line:
+				hasLicense = True
+			if "mtoa" in line:
+				hasLicense = True
+	if hasLicense:
+		with open(file,"r+") as f:
+			new_f = f.readlines()
+			f.seek(0)
+			for line in new_f:
+				if "license" not in line:
+					#added to purge arnold :)
+					if "mtoa" not in line:
+						f.write(line)
+			f.truncate()
+		print("Cleared a license from: ", file, "\n")
+		file_count += 1
 print("Cleared from ", file_count, " files.\n")
